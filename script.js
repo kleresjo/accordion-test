7fca34e71263fb9d0aeeeb0491bebef897411cc3
Julia Kleresjö <92084007+kleresjo@users.noreply.github.com>
2023-11-16 14:04:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// A function that adds and remove the class "active" on the section you click on.
+function faqClickHandler(e) {
+  console.log(e);
+
+  const element = e.currentTarget;
+  element.classList.toggle("myActiveFaq");
+}
+
+// Selects and HTML element, and calls a function which will be executed when the element is clicked.
+const section1Element = document.getElementById("section1");
+section1Element.addEventListener("click", faqClickHandler);
+
+const accordion = document.querySelector(".accordion");
+
+async function getFaqData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  console.log(data);
+
+  data.forEach((post) => {
+    const title = post.title;
+    const body = post.body;
+
+    const titleDiv = document.createElement("div");
+    titleDiv.className = "title";
+
+    titleDiv.addEventListener("click", faqClickHandler);
+
+    const descriptionDiv = document.createElement("div");
+    descriptionDiv.className = "description";
+
+    titleDiv.innerHTML = `<i class="fa-solid fa-chevron-up"></i>
+    <p>${title}</p>`;
+
+    descriptionDiv.textContent = body;
+
+    accordion.appendChild(titleDiv);
+    accordion.appendChild(descriptionDiv);
+  });
+}
+getFaqData();
